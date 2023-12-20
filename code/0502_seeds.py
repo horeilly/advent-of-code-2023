c018001ece9fd58b47ad1511975034a2d1e19872
Harry O'Reilly <Harry@harrys-mbp.home>
2023-12-20 23:59:57 +0000

    day 5, part 2 bs solution added

--- code/0502_seeds.py
+++ code/0502_seeds.py
@@ -1,42 +1,8 @@
-import cProfile
+# BS solution; manually changing ranges and iterating
 
 def read_input() -> list[str]:
     with open("../data/05_seeds.txt", "r") as f:
         text = f.read()[:-1].split("\n\n")
-#     text = """seeds: 79 14 55 13
-#
-# seed-to-soil map:
-# 50 98 2
-# 52 50 48
-#
-# soil-to-fertilizer map:
-# 0 15 37
-# 37 52 2
-# 39 0 15
-#
-# fertilizer-to-water map:
-# 49 53 8
-# 0 11 42
-# 42 0 7
-# 57 7 4
-#
-# water-to-light map:
-# 88 18 7
-# 18 25 70
-#
-# light-to-temperature map:
-# 45 77 23
-# 81 45 19
-# 68 64 13
-#
-# temperature-to-humidity map:
-# 0 69 1
-# 1 0 69
-#
-# humidity-to-location map:
-# 60 56 37
-# 56 93 4
-# """[:-1].split("\n\n")
     return text
 
 
@@ -45,6 +11,8 @@ def build_seed_map(text: list[str]) -> list[list[int]]:
     config = [int(s) for s in text[0].split(": ")[1].split(" ")]
     for i in range(0, len(config), 2):
         output.append([config[i], config[i] + config[i + 1]])
+    for row in output:
+        print(row)
     return output
 
 
@@ -80,26 +48,30 @@ def main() -> None:
     l2h_map = parse_map(text[7])
 
     i = 1
+    power = 8
+    bounds = [0, 10]
+    # for i in range(50855030, 50855040, 1):
     while True:
-        if i % 100000 == 0:
-            print(i)
-            break
-        state = i
-        for map_ in [l2h_map, h2t_map, t2l_map, l2w_map, w2f_map, f2s_map, s2s_map]:
-            state = use_map(state, map_)
-        if check_valid_seed(seed_map, state):
-            print(i, state)
-            break
-        else:
-            i += 1
+        for i in range(*bounds):
+            # if i % 1000 == 0:
+            #     print(i)
+            #     break
+            state = i * (10 ** power)
+            for map_ in [l2h_map, h2t_map, t2l_map, l2w_map, w2f_map, f2s_map, s2s_map]:
+                state = use_map(state, map_)
+            if check_valid_seed(seed_map, state):
+                print("DING DING DING")
+                power -= 1
+                bounds = i // (10 ** power), (i + 1) // (10 ** power)
+                break
+
+        print(i, state)
+            # break
+        # else:
+        #     pass
+            # i += 1
     return None
 
 
 if __name__ == "__main__":
-    cProfile.run("main()")
-
-# 39  0 15
-#  0 15 35
-# 37 50  2
-# 54 52 46
-# 35 98  2
+    main()
